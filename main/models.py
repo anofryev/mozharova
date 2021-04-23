cd229b54894894610ec078a2a4f848c83ea7e8bf
anofryev <anofryev@gmail.com>
2021-04-23 23:58:32 +0300

    models added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Painting(models.Model):
+    """Картина"""
+    title_rus = models.CharField("Название", max_length=100)
+    title_eng = models.Charfield("Title", max_length=100, null=True)
+    description_rus = models.CharField("Описание", max_length=2000, null=True)
+    description_eng = models.CharField("Description", max_length=2000, null=True)
+    year = models.PositiveIntegerField("Год", default=2021)
+    tags = models.ManyToManyField()##
+
+
+class Tags(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
